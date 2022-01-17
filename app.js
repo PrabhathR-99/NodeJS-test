420536311dc5cf97eb2a6be3f55e663e4a11dbd4
prabathr651@gmail.com <prabathr651@gmail.com>
2022-01-18 02:24:38 +0530

    Html requests and responces

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
  if(req.url === '/'){
      res.end('welcome to our homepage')
- }
+ }  
     if (req.url === '/about'){
         res.end('Here is our short history')
     }
