b5111414bab4440e27c0d682521656f26e12e65b
Prabhath Rathnayaka <prabathr651@gmail.com>
2022-01-21 21:53:28 +0530

    Second commit

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-const http = require('http');
+const_= require('lodash')
 
-const server = http.createServer((req,res)=>{
- if(req.url === '/'){
-     res.end('welcome to our homepage')
- }  
-    if (req.url === '/about'){
-        res.end('Here is our short history')
-    }
-
-    res.end(`
-        <h1>Oops!</h1>
-        <p>we cant seem to find</p>
-        <a href="/">back home</a>
-    `)
-})
-
-server.listen(3000)
+const items = [1,[2,[3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems);
